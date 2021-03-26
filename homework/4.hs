50c2c11f0cb96eac17f9fc0355d15730e699c1ae
alekseik1 <1alekseik1@gmail.com>
2021-03-26 18:31:36 +0300

    hw: task 4

---
+++ homework/4.hs
@@ -0,0 +1,10 @@
+replicate' :: Int -> a -> [a]
+replicate' 0 x = []
+replicate' n x
+    | n >= 0 = [x] ++ replicate (n-1) x
+    | otherwise = []
+
+repeat' :: a -> [a]
+repeat' x = [x] ++ repeat x
+
+main = print ("ok")
