7ff0f93eed91a7315c2c64dfb760ee352cd67641
alekseik1 <1alekseik1@gmail.com>
2021-04-09 23:14:44 +0300

    hw: task 19

---
+++ homework/19.hs
@@ -0,0 +1,16 @@
+-- auxiliary function
+upart' _ 0 = [[]]
+upart' 0 _ = [[]]
+upart' 1 _ = [[1]]
+upart' n 1 = [take n [1,1..]]
+upart' target max_elem = concat [map (\l -> [x] ++ l) (upart' (target - x) x) | x <- [lim',(lim'-1)..1]]
+    where
+    lim' = min target max_elem
+
+upart n = upart' n n
+-- *Main> upart 5
+-- [[5],[4,1],[3,2],[3,1,1],[2,2,1],[2,1,1,1],[1,1,1,1,1]]
+-- *Main> upart 7
+-- [[7],[6,1],[5,2],[5,1,1],[4,3],[4,2,1],[4,1,1,1],[3,3,1],[3,2,2],[3,2,1,1],[3,1,1,1,1],[2,2,2,1],[2,2,1,1,1],[2,1,1,1,1,1],[1,1,1,1,1,1,1]]
+
+main = print "ok"
